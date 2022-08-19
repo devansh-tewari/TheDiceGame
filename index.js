1f128f38885ad0b263e6124780e549bfaa85f842
Devansh Tewari <74170553+tewari-devansh@users.noreply.github.com>
2022-08-19 16:39:13 +0530

    Update index.js

--- index.js
+++ index.js
@@ -3,9 +3,11 @@ var a = Math.random();
 var b = Math.random();
 a = Math.floor(a*6)+1;
 b = Math.floor(b*6)+1;
+var c = "dice"+a+".png";
+var d = "dice"+b+".png";
 document.querySelector(".btn").textContent="Roll Again";
-document.querySelector(".fst img").setAttribute("src", "dice"+a+".png");
-document.querySelector(".scnd img").setAttribute("src", "dice"+b+".png");
+document.querySelector(".fst img").setAttribute("src", c);
+document.querySelector(".scnd img").setAttribute("src", d);
 }
 if(a>b){
     document.querySelector("h1").textContent="PLAYER 1 WON";
