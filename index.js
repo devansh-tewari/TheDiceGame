b0bb4555f1217e671631f08d63b684d610aa08a8
Devansh Tewari <74170553+tewari-devansh@users.noreply.github.com>
2022-08-22 13:14:34 +0530

    Update index.js

--- index.js
+++ index.js
@@ -2,19 +2,25 @@ var sum1=0,sum2=0;
 document.querySelector(".btn2").disabled = true;
 function check(){
     if(sum1>=30){
+      document.querySelector("h1").style.color="#B2A4FF";
+      document.querySelector("h1").style.backgroundColor="#411530";
       document.querySelector("h1").textContent="Player 1 Won";
       document.querySelector(".btn1").textContent="Game Over";
       document.querySelector(".btn2").textContent="Game Over";
       document.querySelector(".btn1").disabled="true";
       document.querySelector(".btn2").disabled="true";
+      document.querySelector(".removeme").style.display="none";  
       document.querySelector(".spn1").textContent="Game Over, Reload to Play Again";
     }
     if(sum2>=30){
+      document.querySelector("h1").style.color="#B2A4FF";
+      document.querySelector("h1").style.backgroundColor="#411530";
       document.querySelector("h1").textContent="Player 2 Won";
       document.querySelector(".btn1").textContent="Game Over";
       document.querySelector(".btn2").textContent="Game Over";
       document.querySelector(".btn1").disabled="true";
       document.querySelector(".btn2").disabled="true";
+      document.querySelector(".removeme").style.display="none";
       document.querySelector(".spn1").textContent="Game Over, Reload to Play Again";
     }
 }
